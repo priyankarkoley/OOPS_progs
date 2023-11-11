a60c0c56539bd7698077653a91aa8da3e4ba6f99
Priyankar Lappy <priyankarkoley@gmail.com>
2023-11-11 17:51:59 +0530

    static in interface experimented

--- 4_Interface/1__Static/Stat.java
+++ 4_Interface/1__Static/Stat.java
@@ -7,6 +7,9 @@ interface Drawable {
     default void speak() {
         System.out.println("quak quak..!!");
     }
+
+    //static methods in interface are not inherited(intrface to another interface) & not implemented(can not be overridden to subclass) and can be called directly by Drawable.run()
+    static void run(){System.out.println("drawbale running!!\n");}
 }
 
 class Rectangle implements Drawable {
@@ -17,22 +20,30 @@ class Rectangle implements Drawable {
     public void speak() {
         System.out.println("pak pak..!!");
     }
+    //this is a seperate static method and can be called by Rectangle.run
+    public static void run(){System.out.println("rect running!!\n");}
 }
 
 class Circle implements Drawable {
     public void draw() {
         System.out.println("Drawing Circle");
     }
+    //this is a seperate static method and can be called by Circle.run
+    public static void run(){System.out.println("circ running!!\n");}
+
 }
 
 public class Stat {
     public static void main(String[] args) {
         Drawable obj;
+        Drawable.run();
         obj = new Rectangle();
         obj.draw();
         obj.speak();
+        Rectangle.run();
         obj = new Circle();
         obj.draw();
         obj.speak();
+        Circle.run();
     }
 }
