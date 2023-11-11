df717547511424ef4957e69153415cd2ffd40126
Priyankar Lappy <priyankarkoley@gmail.com>
2023-11-11 15:38:34 +0530

    super param

--- 2_Inheritance/3__Super_Constructor/Oride.java
+++ 2_Inheritance/3__Super_Constructor/Oride.java
@@ -1,21 +1,32 @@
-class Base{
-    public int a,b;
-    Base(){System.out.println("Base Constructor Called\n");a=5;b=6;}
+class Base {
+    public int a, b;
 
-    void show(){
+    Base(int a, int b) {
+        System.out.println("Base Constructor Called\n");
+        this.a = a;
+        this.b = b;
+    }
+
+    void show() {
         System.out.println("Hi from the BASEEE Class!!");
-        System.out.println("a = "+a+" b = "+b);
+        System.out.println("a = " + a + " b = " + b);
     }
 }
 
-class Sub extends Base{
-    public int c,d;
-    Sub(){System.out.println("Sub Constructor Called\n");c=7;d=8;}
-    
-    void show(){
+class Sub extends Base {
+    public int c, d;
+
+    Sub(int a, int b) {
+        super(a,b);
+        System.out.println("Sub Constructor Called\n");
+        c = 7;
+        d = 8;
+    }
+
+    void show() {
         super.show();
         System.out.println("Hi from the SUBBBB Class!!");
-        System.out.println("c = "+c+" d = "+d);
+        System.out.println("c = " + c + " d = " + d);
     }
 }
 
@@ -23,7 +34,7 @@ public class Oride {
     public static void main(String[] args) {
         // Base ba = new Base();
         // ba.show();
-        Sub su = new Sub();
+        Sub su = new Sub(5,6);
         su.show();
     }
 }
